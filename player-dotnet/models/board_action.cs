057573cb1efcc6febdb47fa7f4e9bc7461bb6698
dmlr1996 <getthopapst@hotmail.de>
2024-05-17 23:19:59 +0200

    added board_action.cs

---
+++ player-dotnet/models/board_action.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using player_dotnet.models;
+
+namespace player_dotnet.models
+{
+    public class BoardAction
+    {
+        public UInt32 Src = 0;
+        public UInt32 Dest = 0;
+        public UInt32 Amount = 0;
+        public Guid Uuid = new Guid();
+        public UInt32 Player = 0;
+        public Progress Progress = new Progress();
+    }
+}
