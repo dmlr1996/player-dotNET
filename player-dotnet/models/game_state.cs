a232ccbd56355b3c1fa6d3671a1d2b975f141ae2
dmlr1996 <getthopapst@hotmail.de>
2024-05-17 23:40:02 +0200

    Added game_state class.

---
+++ player-dotnet/models/game_state.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_dotnet.models
+{
+    public class GameState
+    {
+        public List<BoardAction> Actions = new List<BoardAction>();
+        public List<Base> Bases = new List<Base>();
+        public GameConfig Config = new GameConfig();
+        public Game Game = new Game();
+    }
+}
