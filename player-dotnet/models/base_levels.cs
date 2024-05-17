c212c41eb832e5230f911486869f091f4db665e5
dmlr1996 <getthopapst@hotmail.de>
2024-05-17 23:13:11 +0200

    added base_levels

---
+++ player-dotnet/models/base_levels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_dotnet.models
+{
+    public class BaseLevels
+    {
+        public UInt32 MaxPopulation = 0;
+        public UInt32 UpgradeCost = 0;
+        public UInt32 SpawnRate = 0;
+    }
+}
