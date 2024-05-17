c54fb8d763a663d8e2042c1a6bc169b62b91a40b
dmlr1996 <getthopapst@hotmail.de>
2024-05-18 00:51:22 +0200

    Updated to Webapp

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_dotnet
+{
+    public class Program
+    {
+        var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapPost("/", (System.Text.Json.JsonElement body) => "{\"bet\": \"" + Strategy.decide(Newtonsoft.Json.JsonConvert.DeserializeObject<Table>(body.ToString())).bet + "\"}");
+
+        app.MapGet("/", () => "Player C#/.net");
+
+
+        app.Run();
+    }
+}
--- models/base.cs
+++ models/base.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using player_dotnet.models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using player_dotnet.models;
 
 namespace player_dotnet.models
 {
