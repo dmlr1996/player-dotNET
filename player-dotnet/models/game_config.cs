c207a4f817516b1d4a40f9466efd952863336306
dmlr1996 <getthopapst@hotmail.de>
2024-05-17 23:38:25 +0200

    Added game_config class.

---
+++ player-dotnet/models/game_config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_dotnet.models
+{
+    public class GameConfig
+    {
+        public List<BaseLevel> BaseLevels = new List<BaseLevel>();
+        public PathConfig Paths = new PathConfig();
+    }
+}
