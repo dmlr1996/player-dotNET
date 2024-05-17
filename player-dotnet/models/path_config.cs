77ba751c353700f195cf79b45d24e4e14ee3bcd1
dmlr1996 <getthopapst@hotmail.de>
2024-05-17 22:58:19 +0200

    Added path_config model

---
+++ player-dotnet/models/path_config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_dotnet.models
+{
+    public class PathConfig
+    {
+        public UInt32 GracePeriod = 0;
+        public UInt32 DeathRate = 0;
+    }
+}
