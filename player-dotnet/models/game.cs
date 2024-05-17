a9c697ab8e67fd507ec63986ae8d653832cd4a9e
dmlr1996 <getthopapst@hotmail.de>
2024-05-17 23:23:21 +0200

    added game.cs

---
+++ player-dotnet/models/game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_dotnet.models
+{
+    public class Game
+    {
+        public UInt32 Uid = 0;
+        public UInt32 Tick = 0;
+        public UInt32 PlayerCount = 0;
+        public UInt32 RemainingPlayers = 0;
+        public UInt32 Player = 0;
+    }
+}
