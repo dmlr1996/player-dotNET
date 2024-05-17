7251ca2144068439c6f66817ce974f1d81d82776
dmlr1996 <getthopapst@hotmail.de>
2024-05-17 23:08:48 +0200

    Added base model.

---
+++ player-dotnet/models/base.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using player_dotnet.models;
+
+namespace player_dotnet.models
+{
+    public class Base
+    {
+        public Position Position = new Position();
+        public UInt32 Uid = 0;
+        public UInt32 Player = 0;
+        public UInt32 Population = 0;
+        public UInt32 Level = 0;
+        public UInt32 UnitsUntilUpgrade = 0;
+    }
+}
