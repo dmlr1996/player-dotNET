6c5e6ee0946e90200c72f1f07a8b0c76fc16ef68
dmlr1996 <getthopapst@hotmail.de>
2024-05-17 22:56:45 +0200

    Added player_action model

---
+++ player-dotnet/models/player_action.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_dotnet.models
+{
+    public class PlayerAction
+    {
+        public UInt32 Src = 0;
+        public UInt32 Dest = 0;
+        public UInt32 Amount = 0;
+    }
+}
