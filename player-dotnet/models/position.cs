1a8b1846f6d9a35a239ac36910b980d7c22e3a04
dmlr1996 <getthopapst@hotmail.de>
2024-05-17 22:54:18 +0200

    Added position model.

---
+++ player-dotnet/models/position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_dotnet.models
+{
+    public class Position
+    {
+        public Int32 X = 0;
+        public Int32 Y = 0;
+        public Int32 Z = 0;
+    }
+}
