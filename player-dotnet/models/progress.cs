bdacd41fc54221785ebdafdff2e0bcafac51690c
dmlr1996 <getthopapst@hotmail.de>
2024-05-17 22:54:30 +0200

    Added progress model

---
+++ player-dotnet/models/progress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_dotnet.models
+{
+    public class Progress
+    {
+        public UInt32 Distance = 0;
+        public UInt32 Traveled = 0;
+    }
+}
