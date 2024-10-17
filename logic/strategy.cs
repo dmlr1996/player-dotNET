c3d6771cca3d9573e41a92503cea7ebd88e6cefd
dmlr1996 <getthopapst@hotmail.de>
2024-10-17 22:36:35 +0200

    Renamed. Fixed naming to convention.

--- logic/strategy.cs
+++ logic/strategy.cs
@@ -1,7 +1,13 @@
-﻿namespace player_dotnet.logic
+﻿using player_dotnet.models;
+
+namespace player_dotnet.logic
 {
     public class Strategy
     {
-
+        public PlayerAction Decide(GameState gameState)
+        {
+            //TODO: Add your logic here!
+            return new PlayerAction();
+        }
     }
 }
