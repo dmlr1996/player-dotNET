ab8186b4637352609736e5dc1ca6e9f7730df28e
structix <janek@schoff.it>
2024-11-15 12:35:06 +0100

    Add base name property

--- models/base.cs
+++ models/base.cs
@@ -4,23 +4,25 @@ namespace PlayerDotNet.models
 {
     public class Base
     {
-        
         [JsonProperty("position")]
         public Position Position { get; set; }
-        
+
         [JsonProperty("uid")]
         public UInt32 Uid { get; set; }
-        
+
         [JsonProperty("player")]
         public UInt32 Player { get; set; }
-        
+
         [JsonProperty("population")]
         public UInt32 Population { get; set; }
-        
+
         [JsonProperty("level")]
         public UInt32 Level { get; set; }
-        
+
         [JsonProperty("units_until_upgrade")]
         public UInt32 UnitsUntilUpgrade { get; set; }
+
+        [JsonProperty("name")]
+        public UInt32 Name { get; set; }
     }
 }
